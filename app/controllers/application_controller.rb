a22476781b35bace026030445da8a5e65454afc1
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-17 22:26:59 +0100

    Update the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_if_signed_in
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+
+  def redirect_if_signed_in
+    return unless user_signed_in? && current_page?(root_path)
+
+    redirect_to categories_path
   end
 end
