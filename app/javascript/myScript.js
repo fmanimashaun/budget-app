c0e680d02db6655795b91fcd79d78155a8a2af09
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-19 13:03:54 +0100

    Update notification message

--- app/javascript/myScript.js
+++ app/javascript/myScript.js
@@ -5,10 +5,11 @@ document.addEventListener("turbo:load", () => {
   const content = document.querySelector("#content");
   const menuBar = document.querySelector("#menubar");
   const avatar = document.querySelector("#avatar");
+  const toast = document.querySelector("#toast");
 
   var img = new Image();
 
-  img.src = 'https://avatar.iran.liara.run/public';
+  img.src = "https://avatar.iran.liara.run/public";
 
   img.onload = function () {
     avatar.src = img.src;
@@ -22,4 +23,12 @@ document.addEventListener("turbo:load", () => {
     content.classList.toggle("off-canvas");
     menuBar.classList.toggle("close");
   });
+
+  // show toast if it has a message
+  if (toast.innerText.trim() !== "") {
+    toast.classList.remove("hidden");
+    setTimeout(() => {
+      toast.classList.add("hidden");
+    }, 5000); // The toast will be hidden after 5 seconds
+  }
 });
