28cf45bbd37e2617b11d7a1aca307c0f2c5e248e
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-19 10:04:10 +0100

    update the navigation functionality

--- app/javascript/myScript.js
+++ app/javascript/myScript.js
@@ -1,23 +1,25 @@
-// get dom element
+document.addEventListener("turbo:load", () => {
+  console.log("Page loaded with Turbo Drive");
 
-const menuBtn = document.querySelector("#menu-btn");
-const content = document.querySelector("#content");
-const menuBar = document.querySelector("#menubar");
-const avatar = document.querySelector("#avatar");
+  const menuBtn = document.querySelector("#menu-btn");
+  const content = document.querySelector("#content");
+  const menuBar = document.querySelector("#menubar");
+  const avatar = document.querySelector("#avatar");
 
-var img = new Image();
+  var img = new Image();
 
-img.src = 'https://avatar.iran.liara.run/public';
+  img.src = 'https://avatar.iran.liara.run/public';
 
-img.onload = function () {
-  avatar.src = img.src;
-};
-img.onerror = function () {
-  avatar.src = avatar.dataset.avatarPath;
-};
+  img.onload = function () {
+    avatar.src = img.src;
+  };
+  img.onerror = function () {
+    avatar.src = avatar.dataset.avatarPath;
+  };
 
-// add event listener
-menuBtn.addEventListener("click", () => {
-  content.classList.toggle("off-canvas");
-  menuBar.classList.toggle("close");
+  // add event listener
+  menuBtn.addEventListener("click", () => {
+    content.classList.toggle("off-canvas");
+    menuBar.classList.toggle("close");
+  });
 });
