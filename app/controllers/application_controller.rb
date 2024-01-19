6a2450e49f49df9e08e6d410ce6ce3403f020e98
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-19 16:56:17 +0100

    Revert "Project completion"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :redirect_if_signed_in
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
-
-  private
-
-  def redirect_if_signed_in
-    redirect_to categories_path if user_signed_in? && request.path == root_path
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :categories do
-    resources :expenses, only: [:index, :new, :create], controller: 'expenses'
-  end
-
   root "home#index"
 end
