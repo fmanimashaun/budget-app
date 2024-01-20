cd139d9c4e2ec921038144ddbf08f3d11729da73
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-20 20:22:24 +0100

    Update expense system test

--- spec/system/expenses/expenses_indices_spec.rb
+++ spec/system/expenses/expenses_indices_spec.rb
@@ -1,42 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe 'Expenses', type: :system do
+RSpec.describe 'Expense Index', type: :system do
   let(:user) { create(:user) }
   let(:category) { create(:category, user: user) }
-  let!(:expenses) { create_list(:expense, 3, category: category, user: user) }
+  let!(:expenses) { create_list(:expense, 3, user: user, categories: [category]) }
 
   before do
     driven_by(:rack_test)
     sign_in user
+    visit categories_path
+    click_on category.name
   end
 
-  describe 'index view' do
-    it 'displays all expenses for the category' do
-      visit category_expenses_path(category)
-      expenses.each do |expense|
-        expect(page).to have_content(expense.name)
-        expect(page).to have_content(number_to_currency(expense.amount))
-        expect(page).to have_content(expense.created_at.strftime('%d %b %Y'))
-      end
-    end
+  it 'displays the category name and total transactions' do
+    expect(page).to have_content(category.name)
+    expect(page).to have_content(number_to_currency(expenses.sum(:amount)))
+  end
 
-    it 'displays a message when there are no expenses' do
-      Expense.destroy_all
-      visit category_expenses_path(category)
-      expect(page).to have_content('No transactions yet')
+  it 'displays each expense' do
+    expenses.each do |expense|
+      expect(page).to have_content(expense.name)
+      expect(page).to have_content(number_to_currency(expense.amount))
+      expect(page).to have_content(expense.updated_at.strftime('%d %b %Y'))
     end
   end
 
-  describe 'new view' do
-    it 'creates a new expense with a category and redirects to categories path' do
-      visit category_expenses_path(category)
-      click_on 'ADD TRANSACTION'
-      fill_in 'Name', with: 'Test Expense'
-      fill_in 'Amount', with: 50.00
-      select category.name, from: 'category_ids[]'
-      click_on 'Add'
-      expect(page).to have_content('Expense was successfully created.')
-      expect(page).to have_current_path(categories_path)
-    end
+  it 'displays a link to add a new transaction' do
+    expect(page).to have_link('ADD TRANSACTION', href: new_category_expense_path(category))
   end
 end
