723077f749b709c7ee50a72abdcf0032558b58e8
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-20 13:28:31 +0100

    Add unit tests for the models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { build(:category) }
+
+  it 'is valid with valid attributes' do
+    expect(category).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    category.name = nil
+    expect(category).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    category.icon = nil
+    expect(category).to_not be_valid
+  end
+
+  it 'has many category_expenses' do
+    expect(Category.reflect_on_association(:category_expenses).macro).to eq(:has_many)
+  end
+
+  it 'has many expenses through category_expenses' do
+    expect(Category.reflect_on_association(:expenses).macro).to eq(:has_many)
+  end
 end
