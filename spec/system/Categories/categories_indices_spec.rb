3641f8377efc19282f529003886deb567b04af22
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-01-20 19:40:17 +0100

    Fix the error in category system test

--- spec/system/Categories/categories_indices_spec.rb
+++ spec/system/Categories/categories_indices_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe 'Categories', type: :system do
     it 'creates a new category' do
       visit new_category_path
       fill_in 'Name', with: 'Test Category'
-      attach_file('category[icon]', "#{Rails.root}spec/support/assets/test-image.png")
+      attach_file('category[icon]', "#{Rails.root}/spec/support/assets/test-image.png")
       click_on 'Add'
       expect(page).to have_content('Category was successfully created.')
       expect(page).to have_content('Test Category')
